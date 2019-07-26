dd775ac443d056a8b4c322d4ad8d09aae5e318ce
Karol Meksuła <meksula.karol.93@gmail.com>
2019-07-26 23:50:16 +0200

    HURRICANE-0 Hurricane Documentation Project generated

---
+++ src/main/java/com/hurricane/generator/Main.java
@@ -0,0 +1,16 @@
+package com.hurricane.generator;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
